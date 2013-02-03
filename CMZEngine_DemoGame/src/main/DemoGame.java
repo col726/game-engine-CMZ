2169752252e7f7fc9ce48abacd096cb098583e4d
ColinM <col726@gmail.com>
2013-02-03 16:09:55 -0500

    few more class revisions

--- CMZEngine_DemoGame/src/main/DemoGame.java
+++ CMZEngine_DemoGame/src/main/DemoGame.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.awt.event.*;
+
 public class DemoGame {
 
 	/**
@@ -7,7 +9,51 @@ public class DemoGame {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		CMZEngine DemoEngine = new CMZEngine();
+		
+		boolean timeForUpdatingAI = true;
+		boolean  timeForUpdatingPhysics = true;
+		boolean timeForRendering = true;
+		boolean quit = false;
+		
+		DemoEngine.Init();
+		
+		while(!quit)
+		{
+			pollForOSMessages();
+			quit = getInput();
+			if(timeForUpdatingAI)
+				DemoEngine.UpdateAI();
+			if(timeForUpdatingPhysics)
+				DemoEngine.UpdatePhysics();
+			updateStatistics();
+			if(timeForRendering)
+				DemoEngine.Render();
+			FPSControl();
+		}
 
+	}
+
+	private static boolean getInput() {
+		// TODO Auto-generated method stub
+		
+		return false;
+	}
+
+	private static void updateStatistics() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void FPSControl() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void pollForOSMessages() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
