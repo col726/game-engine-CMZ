266caad178eaa217aa858a4e176888561141cbb4
Zach Smoak <zsmoak@gmail.com>
2013-02-07 17:46:06 -0500

    basic start to the game loop implementation

--- CMZEngine_DemoGame/src/main/DemoGame.java
+++ CMZEngine_DemoGame/src/main/DemoGame.java
@@ -4,6 +4,7 @@ import java.awt.event.*;
 
 public class DemoGame {
 
+	private boolean quit = false;
 	/**
 	 * @param args
 	 */
@@ -13,24 +14,35 @@ public class DemoGame {
 		CMZEngine DemoEngine = new CMZEngine();
 		
 		boolean timeForUpdatingAI = true;
-		boolean  timeForUpdatingPhysics = true;
+		boolean timeForUpdatingPhysics = true;
 		boolean timeForRendering = true;
-		boolean quit = false;
+		
+		int time = SDL_GetTicks();
+		int REDRAWING_PERIOD = 20;
+		int MAX_FRAME_SKIP = 10;
+		boolean need_to_redraw = true;
+		
+		Object I;
 		
 		DemoEngine.Init();
 		
 		while(!quit)
 		{
 			pollForOSMessages();
-			quit = getInput();
+			I = getInput();
 			if(timeForUpdatingAI)
-				DemoEngine.UpdateAI();
+				DemoEngine.UpdateAI(I);
 			if(timeForUpdatingPhysics)
-				DemoEngine.UpdatePhysics();
+				DemoEngine.UpdatePhysics(I);
 			updateStatistics();
-			if(timeForRendering)
+			if(timeForRendering(DemoEngine, need_to_redraw))
+			{
 				DemoEngine.Render();
+				need_to_redraw = false;
+			}
 			FPSControl();
+			
+			SDL_Delay(1);
 		}
 
 	}
@@ -39,6 +51,22 @@ public class DemoGame {
 		// TODO Auto-generated method stub
 		
 		return false;
+	}
+	
+	private static void timeForRendering(CMZEngine game, boolean need_to_redraw) {
+		int act_time = SDL_GetTicks();
+		int frames = 0;
+		while(act_time - time >= REDRAWING_PERIOD && frames<MAX_FRAME_SKIP) {
+			time += REDRAWING_PERIOD;
+			keyboard->cycle();
+			if (!game->cycle(k)) quit = true;
+			act_time = SDL_GetTicks();
+			need_to_redraw = true;
+			frames++;
+		}
+		if (time < act_time) time = act_time;
+		
+		return need_to_redraw;
 	}
 
 	private static void updateStatistics() {
@@ -53,7 +81,21 @@ public class DemoGame {
 
 	private static void pollForOSMessages() {
 		// TODO Auto-generated method stub
-		
+		while ( event = SDL_PollEvent() ) {
+			switch ( event.type ) {
+				case SDL_KEYDOWN:
+					if (event.key.keysym.sym == SDLK_F12) {
+						quit = true;
+					}
+					break;
+				case SDL_MOUSEBUTTONDOWN:
+					game->MouseClick(event.button.x,event.button.y);
+					break;
+				case SDL_QUIT:
+					quit = true;
+					break;
+			}
+		}
 	}
 
 }
