b92d6ed331ca87f8a485021ea4fd6565a06564da
mp925 <justdrexel@gmail.com>
2013-02-20 15:45:54 -0500

    Latest Commit - Mike

--- AndroidProject/workspace/TestGameLoop/src/MainPkg/MainGameLoop.java
+++ AndroidProject/workspace/TestGameLoop/src/MainPkg/MainGameLoop.java
@@ -1,9 +1,13 @@
 package MainPkg;
+import java.io.IOException;
+
 import javax.swing.JOptionPane;
 public class MainGameLoop {
-	  public static void main(String[] args) {
+	  public static void main(String[] args) throws IOException {
 		    System.out.println("MainGameLoop - Main Function");
 		    JOptionPane.showMessageDialog(null, "Class -  MainGameLoop -- function - main()");
+		    clsCanvas MyCanvas = new clsCanvas();
+		    MyCanvas.MyCanvas();
 		    clsInitialization initmethod = new clsInitialization();
 		    initmethod.InitializeObjects();
      	    clsGameLoop maingameloop = new clsGameLoop();
